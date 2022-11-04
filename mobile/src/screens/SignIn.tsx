5d958c0e0f3fa4e1b17ed4dccaeba0000295a6ae
Alehandro Vidal <alehandrosantos99@gmail.com>
2022-11-04 13:10:54 -0300

    Ambiente Preparado

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { NativeBaseProvider, Text, Center } from "native-base";
+
+export function SignIn() {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} bgColor="gray.900"></Center>
+    </NativeBaseProvider>
+  );
+}
