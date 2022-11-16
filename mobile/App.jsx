573ce8948481d21e7b48c51cb0c345477497da9c
Alehandro Vidal <alehandrosantos99@gmail.com>
2022-11-15 22:47:12 -0300

    Interface mobile pronta

--- mobile/App.jsx
+++ mobile/App.jsx
@@ -6,11 +6,12 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { SignIn } from "./src/screens/SignIn";
+import { Pools } from "./src/screens/Pools";
 
 import { Loading } from "./src/components/Loading";
 
 import { THEME } from "./src/styles/theme";
+import { AuthContextProvider } from "./src/context/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,12 +22,15 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        {fontsLoaded ? <Pools /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
